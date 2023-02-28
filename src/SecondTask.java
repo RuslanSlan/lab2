83f19458013b193632db756b0b8997a9423bea58
RuslanSlan <49315740+RuslanSlan@users.noreply.github.com>
2023-02-28 17:55:49 +0200

    Update SecondTask.java

--- src/SecondTask.java
+++ src/SecondTask.java
@@ -1,6 +1,6 @@
 public class SecondTask {
     public static void main(String[] args) {
         int num = 25;
-        System.out.println("Сума цифр числа: " + (num/10 + num%10));
+        System.out.println("Сумма цифр числа: " + (num/10 + num%10));
     }
 }
